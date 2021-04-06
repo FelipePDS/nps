39a28ebeb75a795cc0dc796ca891d2c001dd0ea1
FelipePDS <felipepdasilva66@gmail.com>
2021-04-06 16:34:07 -0300

    adding server

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,3 @@
 import { app } from "./app";
 
-app.listen(3333, () => console.log("Server is running 🚀 in http://localhost:3333/"))
+app.listen(3333, () => console.log("Server is running 🚀 on http://localhost:3333/"))
