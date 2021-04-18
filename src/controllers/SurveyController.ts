673fb00000447ec21e708137d3358774aa7a373b
FelipePDS <felipepdasilva66@gmail.com>
2021-04-18 20:51:57 -0300

    fix: API validations

--- src/controllers/SurveyController.ts
+++ src/controllers/SurveyController.ts
@@ -1,11 +1,23 @@
 import { Request, Response } from "express"
 import { getCustomRepository } from "typeorm"
 import { SurveyRepository } from "../repositories/SurveyRepository"
+import * as yup from "yup"
 
 class SurveyController {
 
     async create(request: Request, response: Response) {
         const { title, description } = request.body
+
+        const schema = yup.object().shape({
+            title: yup.string().required(),
+            description: yup.string().required()
+        })
+
+        try {
+            await schema.validate(request.body, { abortEarly: false })
+        } catch(err) {
+            return response.status(400).json({ error: err })
+        }
 
         const surveyRepository = getCustomRepository(SurveyRepository)
 
--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,11 +1,23 @@
-import { Request, response, Response } from "express"
+import { Request, Response } from "express"
 import { getCustomRepository } from "typeorm"
 import { UserRepository } from "../repositories/UserRepository"
+import * as yup from "yup"
 
 class UserController {
 
     async create(request: Request, response: Response) {
         const { name, email } = request.body
+
+        const schema = yup.object().shape({
+            name: yup.string().required(),
+            email: yup.string().email().required()
+        })
+
+        try {
+            await schema.validate(request.body, { abortEarly: false })
+        } catch(err) {
+            return response.status(400).json({ error: err })
+        }
 
         const userRepository = getCustomRepository(UserRepository)
 
